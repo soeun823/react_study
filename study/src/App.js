b407d56f06b71a3e01c5319311cb21f72e097067
soeun823 <bsoeun866@gmail.com>
2024-09-10 23:55:24 +0900

    state, useState

--- study/src/App.js
+++ study/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Hello from './component/Hello';
-import styles from './App.module.css';
+
 
 function App() {
 
   return (
     <div className="App">
       <Hello />
-      <div className={styles.box}>App</div>
+      <Hello />
+      <Hello />
     </div>
   );
 }
--- study/src/component/Hello.js
+++ study/src/component/Hello.js
@@ -1,29 +1,14 @@
+import React, { useState } from'react';
 export default function Hello(){
-    function showName(){
-        console.log('Mike');
-    }
-    function showAge(age){
-        console.log(age);
-    }
-    function showText(txt){
-        console.log(txt);
-    }  
+    // let name ="Mike";
+    const [name, setName] = useState('Mike'); 
 
-    return( 
+return( 
         <div>
-            <p>Hello</p>
-            <button onClick={showName}>Show name</button>
-            <button onClick={
-                () => {
-                    showAge(10);
-                }
-            }>Show age</button>
-
-            <input type="text" onChange={(e) => {
-                const txt = e.target.value;
-                showText(txt);
-            }}/>
-
+            <h1 id="name">{name}</h1>
+            <button onClick={ () => {
+                setName(name === "Mike"? "Jane" : "Mike");
+            }}>Change</button> 
         </div>
     )
 }
