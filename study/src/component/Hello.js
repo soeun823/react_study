5829d151d4ca84f0efdc544af90875194d9cfb85
soeun823 <bsoeun866@gmail.com>
2024-09-11 20:16:37 +0900

    props에 대하여

--- study/src/component/Hello.js
+++ study/src/component/Hello.js
@@ -1,12 +1,16 @@
 import React, { useState } from'react';
-export default function Hello(){
-    // let name ="Mike";
+import UserName from './UserName';
+export default function Hello({ age }) {
     const [name, setName] = useState('Mike'); 
+    const msg = age > 19 ? "성인 입니다" : "미성년자 입니다.";
 
 return( 
         <div>
-            <h1 id="name">{name}</h1>
-            <button onClick={ () => {
+            <h1 id="name">
+                {name}({age}) : {msg}
+            </h1>
+            <UserName name={name} />
+            <button onClick={() => {
                 setName(name === "Mike"? "Jane" : "Mike");
             }}>Change</button> 
         </div>
