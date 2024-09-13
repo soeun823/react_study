71915d13ed00fd9c6ed2c35af54ecdc357605031
soeun823 <bsoeun866@gmail.com>
2024-09-13 10:48:27 +0900

    fixed)잘못된useParams 사용

--- study/src/component/Day.js
+++ study/src/component/Day.js
@@ -1,25 +1,22 @@
-import dummy from "../db/data.json"
+import dummy from "../db/data.json";
 import { useParams } from "react-router-dom";
 
-export default function Day(){
-    //dummy.words
-    const {day} = useParams().day;
-    const wordList = dummy.words.filter(word => (word.day === Number(day)))
-    
+export default function Day() {
+const { day } = useParams();
+const wordList = dummy.words.filter(word => word.day === Number(day));
 
-
-    return(
+return (
     <>
-    <h2> Day{day}</h2>
+        <h2>Day {day}</h2>
         <table>
-            <tbody>
-                {wordList.map(word => (
-                    <tr key={word.id}>
-                        <td>{word.eng}</td>
-                        <td>{word.kor}</td>
-                    </tr>
-                ))}
-            </tbody>
+        <tbody>
+            {wordList.map(word => (
+            <tr key={word.id}>
+                <td>{word.eng}</td>
+                <td>{word.kor}</td>
+            </tr>
+            ))}
+        </tbody>
         </table>
     </>
     );
