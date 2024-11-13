fc38fae6ef7775235ce26b8887c1d143934967b8
soeun823 <bsoeun866@gmail.com>
2024-11-13 17:45:46 +0900

    useState란..

--- propsExample/src/App.jsx
+++ propsExample/src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, 카운트변경] = useState(0);
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Counter</h1>
+      <h3>{count}번 눌럿읍니다.</h3>
+      <button onClick={() => 카운트변경(count+1)}>증가</button>
+      <button onClick={() => 카운트변경(count-1)}>감소</button>
+      <button onClick={() => 카운트변경(0)}>초기화</button>
     </>
-  )
+  );
 }
-
 export default App
--- propsExample/src/main.jsx
+++ propsExample/src/main.jsx
@@ -1,34 +1,9 @@
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
 
-function 컴포넌트(props){
-  return (
-    <div>
-      {props.name}님
-      <h1>안녕하세요</h1>
-    </div>
-  );
-}
-
-function 컴포넌트2(props){
-  const  conut = props
-  const user = props
-  return(
-    <div>
-      <h1>{user}님 안녕하세요 {count}번 클릭 하셨읍니다</h1>
-    </div>  
-  );
-}
-
-function Hello(props){
-  return (
-    <main>
-      <h1>heelo {props.name} World</h1>
-      <h3>{number} 번째 방문입다</h3>
-    </main>
-  )
-}
 createRoot(document.getElementById('root')).render(
-  <>
-  <Hello name = "소마고" number = "3"/>
-  </>
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
