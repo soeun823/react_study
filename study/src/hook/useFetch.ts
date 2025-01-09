b8e51bda70888f593ec98c173e877bd0a2f92341
soeun823 <bsoeun866@gmail.com>
2025-01-09 14:59:03 +0900

    axios으로 연동 다시 하기
    fetch로 연동한거 axios코드로 바꿔서 다시 바꿨습니다.

--- study/src/hook/useFetch.ts
+++ study/src/hook/useFetch.ts
@@ -1,16 +1,36 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 
-export default function useFetch(url : string){
+export default function useFetch(url: string) {
     const [data, setData] = useState([]);
 
     useEffect(() => {
-        fetch(url)
-            .then(res => {
-                return res.json();
+        axios.get(url)
+            .then(response => {
+                setData(response.data);
             })
-            .then(data => {
-                setData(data);
+            .catch(error => {
+                console.error("Error fetching data:", error);
             });
     }, [url]);
+
     return data;
 }
+
+//fetch로 구현
+// import { useEffect, useState } from "react";
+
+// export default function useFetch(url : string){
+//     const [data, setData] = useState([]);
+
+//     useEffect(() => {
+//         fetch(url)
+//             .then(res => {
+//                 return res.json();
+//             })
+//             .then(data => {
+//                 setData(data);
+//             });
+//     }, [url]);
+//     return data;
+// }
